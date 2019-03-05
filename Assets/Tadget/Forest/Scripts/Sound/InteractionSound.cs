2fca03565e8bea8ce3ba79dcb40d7922adb2a040
Fester <festersy@gmail.com>
2019-03-05 20:11:03 +0700

    Interactions hooked up with sound

---
+++ Assets/Tadget/Forest/Scripts/Sound/InteractionSound.cs
@@ -0,0 +1,25 @@
+﻿namespace Tadget
+{
+    using UnityEngine;
+
+    public class InteractionSound : MonoBehaviour {
+
+        AudioSource source;
+
+        void Start()
+        {
+            source = GetComponent<AudioSource>();
+            Interactable.Play += PlaySound;
+        }
+
+        private void OnDisable()
+        {
+            Interactable.Play -= PlaySound;
+        }
+
+        void PlaySound()
+        {
+            source.Play();
+        }
+    }
+}
