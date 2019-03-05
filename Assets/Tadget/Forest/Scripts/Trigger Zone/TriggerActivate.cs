215f5b02ceeddb66b1695b452790052002e57d93
Fester <festersy@gmail.com>
2019-03-05 17:07:51 +0700

    Trigger zone finished

---
+++ Assets/Tadget/Forest/Scripts/Trigger Zone/TriggerActivate.cs
@@ -0,0 +1,29 @@
+﻿namespace Tadget
+{
+    using UnityEngine;
+
+    public class TriggerActivate : MonoBehaviour
+    {
+        public GameObject whatCanActivate;
+        public float delayTime = 1f;
+        float delayTimeCounter;
+
+        private void Update()
+        {
+            delayTimeCounter -= Time.deltaTime;
+        }
+
+        private void OnTriggerEnter(Collider trigger)
+        {
+            if (trigger.tag == whatCanActivate.tag)
+            {
+
+                if (delayTimeCounter <= 0)
+                {
+                    GetComponent<Spawn>().Use();
+                    delayTimeCounter = delayTime;
+                }
+            }
+        }
+    }
+}
