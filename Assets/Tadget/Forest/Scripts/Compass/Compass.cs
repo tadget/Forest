825b7cf469552f35fcde78a756498798809a1eb8
Fester <festersy@gmail.com>
2019-03-06 13:39:12 +0700

    Compass now rotates with the player

---
+++ Assets/Tadget/Forest/Scripts/Compass/Compass.cs
@@ -0,0 +1,23 @@
+﻿namespace Tadget
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class Compass : MonoBehaviour
+    {
+        public RawImage compassImage;
+        public Transform player;
+
+        /*private void Start()
+        {
+            compassImage = GetComponent<RawImage>();
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }*/
+
+        private void Update()
+        {
+            compassImage.uvRect = new Rect(player.localEulerAngles.y / 360, 0, 1, 1);
+        }
+    }
+}
+
